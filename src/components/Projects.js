ed6b96ab670324205c8645f9eb934ad907f3bbe7
Alex <1995aha@gmail.com>
2023-02-11 15:02:43 +0000

    finished content for project 1

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,17 @@
-import { Box, Card, Divider, Typography } from "@mui/material"
+import {
+  Box,
+  Card,
+  Divider,
+  Typography,
+  List,
+  ListItem,
+  Grid,
+} from "@mui/material"
 import Accordion from "@mui/material/Accordion"
 import AccordionSummary from "@mui/material/AccordionSummary"
 import AccordionDetails from "@mui/material/AccordionDetails"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import DoneIcon from "@mui/icons-material/Done"
 
 const Projects = () => {
   const styles = {
@@ -18,13 +27,63 @@ const Projects = () => {
       <Box sx={styles.wrapper}>
         <Box sx={styles.project1}>
           <Typography variant="h3">Project 1 - Factored MVP</Typography>
-          <Divider>Overview</Divider>
+          <Divider>
+            <Typography variant="body1">Overview</Typography>
+          </Divider>
           <Typography variant="body1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
-            libero aspernatur? Alias obcaecati suscipit quasi blanditiis
-            voluptatem voluptates ipsam, similique quibusdam reiciendis
-            dignissimos sint incidunt harum veniam totam consequatur id.
+            I built an MVP from 0 for a client that allows landlords to take out
+            loans and use their future property rent as collateral.
           </Typography>
+          <Divider>
+            <Typography variant="body1">Key Features</Typography>
+          </Divider>
+          <List>
+            <ListItem>
+              <Card>
+                <DoneIcon></DoneIcon>
+                User authentication to allow registration, log in and password
+                reset over email{" "}
+              </Card>
+            </ListItem>
+            <ListItem>
+              <Card>
+                <DoneIcon></DoneIcon>
+                Stateful authentication with token authentication
+              </Card>
+            </ListItem>
+            <ListItem>
+              {" "}
+              <Card>
+                <DoneIcon></DoneIcon>
+                Ability to create an Advance (loan) and submit supporting
+                documents
+              </Card>
+            </ListItem>
+            <ListItem>
+              {" "}
+              <Card>
+                <DoneIcon></DoneIcon>
+                Schedule of payments and their statuses are generated once the
+                state of each loan changes
+              </Card>
+            </ListItem>
+            <ListItem>
+              {" "}
+              <Card>
+                <DoneIcon></DoneIcon>
+                Custom admin panel that allows administrators to change loan and
+                payment details, plus 2FA for admin users
+              </Card>
+            </ListItem>
+            <ListItem>
+              {" "}
+              <Card>
+                <DoneIcon></DoneIcon>
+                Third party API integration with Persona for KYC (Know Your
+                Customer) checks and Sendgrid (for emails)
+              </Card>
+            </ListItem>
+          </List>
           <Accordion>
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
@@ -35,9 +94,29 @@ const Projects = () => {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
+                Initially, I worked on creating a frontend site that used only
+                using local storage as the client wanted a barebones app that
+                they could demonstrate to potential investors. Later, as the
+                backend was being built out, I started connecting the frontend
+                to the Rest API to enable end to end functionality and move away
+                from storing app data in local storage.
+                <br></br>
+                <br></br>
+                React – I used React for the frontend functionality and axios
+                library for interacting with the backend API server. When a user
+                logs in, an auth token is saved to local storage and is used
+                with each request to the backend to authenticate it. I also used
+                react router dom to fetch the URL details to identify each
+                advance (loan).
+                <br></br>
+                <br></br>
+                CSS3 - I decided to use vanilla CSS for this MVP because I
+                wanted to revise some of the styling and positioning skills that
+                I learned in 2022. This also gave me the perfect opportunity to
+                practise more advanced layout features like flexbox and grid.
+                The client specified that mobile friendly frontend was not a
+                priority for this MVP, however I still tried my best to design
+                pages that would look good on all screen sizes.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -51,9 +130,18 @@ const Projects = () => {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
+                Django Rest Framework (DRF) – the backend is based on Django and
+                DRF, and for the database, I used PostgreSQL. I configured two
+                third party APIs into the backend – Sendgrid to send emails to
+                existing users that would like to reset their passwords, and
+                created a webhook for Persona (along with IP whitelisting) that
+                would accept a POST request whenever a user would successfully
+                verify their identity on the frontend, which in turn would
+                change the “is_verified” flag in the backend.
+                <br></br>
+                <br></br>I also utilised UUID and short UUID for identifying
+                users and loans, as that is more secure that using sequential
+                IDs that are default in Django.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -67,13 +155,80 @@ const Projects = () => {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
+                The frontend app is deployed on AWS Amplify that syncs with a
+                GitHub repo and adds environment variables. I added a YAML file
+                in the root directory that would cd into the client folder as I
+                initially thought both the backend and the frontend would be in
+                the same repo.
+                <br></br>
+                <br></br>
+                The backed app and databased are set up with AWS Elastic
+                Beanstalk (EB), along with S3 for file storage. The EB
+                environment has a load balancer with a couple of listeners that
+                handle HTTP and HTTPS requests (the HTTPS listener was
+                configured to use SSL certificates held by the main domain of
+                the project).
               </Typography>
             </AccordionDetails>
           </Accordion>
-          <Card></Card>
+          <Divider>
+            <Typography variant="body1">Challenges & Solutions</Typography>
+          </Divider>
+          <Grid container>
+            <Grid item md={6}>
+              <Divider>
+                <Typography variant="body1">Frontend</Typography>
+              </Divider>
+            </Grid>
+            <Grid item md={6}>
+              <Divider>
+                <Typography variant="body1">Backend</Typography>
+              </Divider>
+            </Grid>
+
+            <Grid item md={6}>
+              <Card>
+                <Typography variant="h3">Challenge</Typography>
+                At the beginning, I used `useContext` hook and create a global
+                context that could share data across any pages or components.
+                However, I came across cases where there would be a race
+                condition between the functions executing inside the global
+                context and the async functions in the page/component, which
+                lead to some unnecessary re-renders and API calls.
+              </Card>
+            </Grid>
+            <Grid item md={6}>
+              <Card>
+                <Typography variant="h3">Challenge</Typography>
+                For Persona webhook, I had to implement IP whitelisting to allow
+                requests only from Persona’s range of IP addresses, otherwise
+                the user would be able to change their own KYC status by sending
+                the request directly to the backend. The problem is that each
+                client request is handled by a load balancer and thus, Django
+                sees the IP address of the load balancer, and not the IP address
+                of the client.
+              </Card>
+            </Grid>
+            <Grid item md={6}>
+              <Card>
+                <Typography variant="h3">Solution</Typography>I moved away from
+                using global context and added similar code to each
+                page/component to ensure no race conditions would be present.
+                This is not the most elegant solution and in the future, I plan
+                to refactor the code, and perhaps use React Router Dom or Redux
+                to reduce the amount of states and functions.
+              </Card>
+            </Grid>
+            <Grid item md={6}>
+              <Card>
+                <Typography variant="h3">Solution</Typography>I utilised `
+                'HTTP_X_FORWARDED_FOR'` of the request to fetch the origination
+                IP address of the client and check if that address belongs to a
+                list of whitelisted IP addresses. This way only authorised
+                requests would be successfully processed by the backend.
+              </Card>
+            </Grid>
+          </Grid>
         </Box>
       </Box>
     </Box>
